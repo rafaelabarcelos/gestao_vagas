f5639f0fd14d38bbdeb23fcf286186504786429a
Rafaela Barcelos <rafaela.zappe@gmail.com>
2025-05-20 15:54:36 -0300

    feat(candidate): adiciona repositório CandidateRepository

--- src/main/java/br/com/rafaela/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rafaela/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,21 +1,29 @@
 package br.com.rafaela.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     @NotBlank()
-    @Pattern(regexp = "\\s+", message = "O campo [username] não deve conter espaço")
+    @Pattern(regexp = "^\\S+$", message = "O campo [username] não deve conter espaço")
     private String username;
 
     @Email(message = "O campo (email) deve conter um e-mail válido")
@@ -26,4 +34,6 @@ public class CandidateEntity {
     private String description;
     private String curriculum;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
